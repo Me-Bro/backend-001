9798429d64340117713b45223c1e43e4304057f3
Priyansh <50priyansh.kumar@gmail.com>
2024-09-06 12:39:25 +0530

    installing some packanges, making connection to MongoDB, setting envoirnment variable

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 import express from 'express';
-const app = express();
+import mongoose from 'mongoose';
+import cors from cors;
+import bodyParser from 'body-parser';
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from 'morgan';
 
+/*    Configuration    */
 const PORT = process.env.PORT || 3000;
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("common"));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+/* Mongoose settings */
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected to MongoDB");
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
 });
 
 app.listen(PORT, () => {
+  connect();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
