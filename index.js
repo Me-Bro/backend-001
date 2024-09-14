49ff3666a2481c690e3775a43db36b0804705787
Vibhanshu Rana <68025665+vrana055@users.noreply.github.com>
2024-09-14 15:10:54 +0530

    Add some required packages (#1)
    * installing some packanges, making connection to MongoDB, setting envoirnment variable
    
    * adding jwt authenticator
    
    * fixed some error
    
    * refactor code
    
    ---------
    
    Co-authored-by: Priyansh <50priyansh.kumar@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-import express from 'express';
-const app = express();
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
+/*    Configuration    */
 const PORT = process.env.PORT || 3000;
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+/* Mongoose settings */
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
+mongoose.connection.on("disconnected", () => {
+  console.log("Disconnected to MongoDB");
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
 });
 
 app.listen(PORT, () => {
+  connect();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
